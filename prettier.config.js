ece652430f709ab3f2405194ce9ddf7e04204188
yktt-nuane <tyinnagoyauniversity@gmail.com>
2022-12-12 13:18:20 +0900

    delete default settings from prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,7 @@
 module.exports = {
-  printWidth: 80,
-  tabWidth: 2,
-  useTabs: false,
   semi: false,
   singleQuote: true,
-  quoteProps: 'as-needed',
-  jsxSingleQuote: false,
   trailingComma: 'es5',
-  bracketSpacing: true,
   bracketSameLine: false,
   arrowParens: 'always',
 }
