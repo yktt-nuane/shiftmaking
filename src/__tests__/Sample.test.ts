155a8612e3a692daac1d4bd7a67228da6b945a80
yktt-nuane <tyinnagoyauniversity@gmail.com>
2022-12-17 17:58:03 +0900

    change file name and remove .ts file from __tests__

--- src/__tests__/Sample.test.ts
+++ src/__tests__/Sample.test.ts
@@ -1,10 +1,8 @@
-import { calculate } from '@/__tests__/calculate'
-
 import { describe, expect, it } from 'vitest'
 
 describe('calculate', () => {
   it('1 + 1 = 2', () => {
-    const result = calculate(1, 1)
+    const result = 1 + 1
     expect(result).toBe(2)
   })
 })
--- src/__tests__/calculate.ts
+++
@@ -1,3 +0,0 @@
-export const calculate = (a: number, b: number) => {
-  return a + b
-}
