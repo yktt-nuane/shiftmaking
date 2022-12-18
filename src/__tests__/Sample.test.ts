061fb7a7caf0898cdaeecb5be35c29385d2f7ea9
yktt-nuane <tyinnagoyauniversity@gmail.com>
2022-12-18 14:21:26 +0900

    revise it of test

--- src/__tests__/Sample.test.ts
+++ src/__tests__/Sample.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest'
 
 describe('calculate', () => {
-  it('1 + 1 = 2', () => {
+  it('should return correct sum', () => {
     const result = 1 + 1
     expect(result).toBe(2)
   })
