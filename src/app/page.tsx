0cc2724ef7c3ba48ad119f10dc1d0c765183d0a6
yktt-nuane <tyinnagoyauniversity@gmail.com>
2023-01-11 13:42:20 +0900

    test: 環境別のログを確認するための、場合分け用ソースコード作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 import styles from '@/styles/Home.module.css'
 
+import { log } from 'next-axiom'
+
 export default function Home() {
   const LOG_TEST = process.env.LOG_TEST
+
+  if (LOG_TEST) {
+    log.info(`This log is output from Vercel: ${LOG_TEST}.`)
+  } else {
+    log.info('This log is output from Unknown.')
+  }
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
