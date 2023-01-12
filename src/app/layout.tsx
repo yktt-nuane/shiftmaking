25dfc875307a387af147ff18ebbe05729fbc16d8
yktt-nuane <tyinnagoyauniversity@gmail.com>
2023-01-12 10:24:48 +0900

    test: 環境変数を変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,8 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  if (process.env.NEXT_PUBLIC_VERCEL_ENV) {
-    log.info(
-      `This log is output from Vercel: ${process.env.NEXT_PUBLIC_VERCEL_ENV}.`
-    )
+  if (process.env.LOG_TEST) {
+    log.info(`This log is output from Vercel: ${process.env.LOG_TEST}.`)
   } else {
     log.info('This log is output from Unknown.')
   }
